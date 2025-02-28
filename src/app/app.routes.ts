d39b246ac562b78d2526835cb01a84750ae3a6de
miketoq <michael.toquam@ndus.edu>
2025-02-28 17:19:33 -0600

    Integrated details page into the applcation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,9 +8,14 @@ path: '',
 component: HomeComponent,
 title: 'Home page',
 },
+// {
+// path: 'details',
+// component: DetailsComponent,
+// title: 'Home Details'
+// },
 {
-path: 'details',
-component: DetailsComponent,
-title: 'Home Details'
-},
+    path: 'details/:id',
+    component: DetailsComponent,
+    title: 'Home Details'
+}
 ];
